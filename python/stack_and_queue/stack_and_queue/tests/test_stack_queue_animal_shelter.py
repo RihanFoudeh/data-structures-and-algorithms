26676dc301590a03ddcf2c361c732ba7b50e239c
rihan <Rihanfoudeh@gmail.com>
2021-11-15 20:54:31 +0200

    solve challange 12

---
+++ python/stack_and_queue/stack_and_queue/tests/test_stack_queue_animal_shelter.py
@@ -0,0 +1,40 @@
+from stack_and_queue.stack_queue_animal_shelter import (AnimalShelter,Node,Queue)
+
+def test_enqueue():
+    actual = []
+    animal = AnimalShelter()
+    animal.enqueue('bull', 'dog')
+    actual += [animal.dog.peek()]
+    animal.enqueue('soker', 'cat')
+    actual += [animal.cat.peek()]
+    excepted = ['bull', 'soker']
+    assert actual == excepted
+
+
+# ####################################################################
+
+def test_enqueue_fail():
+    animal = AnimalShelter()
+    actual = animal.enqueue('pummy', 'hamster')
+    excepted = 'This is not a cat or a dog'
+    assert actual == excepted
+
+
+# ####################################################################
+
+def test_dequeue():
+    animal = AnimalShelter()
+    animal.enqueue('bull', 'dog')
+    animal.enqueue('soker', 'cat')
+    actual = [animal.dequeue('dog'), animal.dequeue('cat')]
+    excepted = ['bull', 'soker']
+    assert actual == excepted
+
+
+# ####################################################################
+
+def test_dequeue_fail():
+    animal = AnimalShelter()
+    actual = animal.dequeue('rabbit')
+    excepted = 'This is not a cat or a dog'
+    assert actual == excepted
